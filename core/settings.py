7197e3c5ee54bacc77d85cc517404aae5598e9e8
HAPIIIS <hafizh.mamoru123@gmail.com>
2024-04-16 20:57:27 +0700

    First commit

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,6 @@ if RENDER_EXTERNAL_HOSTNAME:
 # Application definition
 
 INSTALLED_APPS = [
-    'admin_soft.apps.AdminSoftDashboardConfig',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -147,8 +146,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/home/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 #if not DEBUG:
 #    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- home/apps.py
+++ home/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
 
-class HomeConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "home"
+class AdminSoftDashboardConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'admin_soft'
+    icon = 'fa fa-user'
